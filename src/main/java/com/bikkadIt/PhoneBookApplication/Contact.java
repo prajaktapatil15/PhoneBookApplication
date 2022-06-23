2b8ef6985173d720c63aaed4e7aabc66a5da5cb1
prajaktapatil15 <prajaktapatil15700@gmail.com>
2022-06-23 12:58:08 +0530

    PHONE 05: create Entity class

--- src/main/java/com/bikkadIt/PhoneBookApplication/Contact.java
+++ src/main/java/com/bikkadIt/PhoneBookApplication/Contact.java
@@ -1,5 +1,115 @@
 package com.bikkadIt.PhoneBookApplication;
 
-public class Contact {
+import java.time.LocalDate;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="CONTACT-DTLS")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="CONTACT_ID")
+	private int contactId;
+    
+	@Column(name="CONTACT_NAME")
+	private String contactName;
+	
+    @Column(name="CONTACT_NUMBER")
+    private Long contactNumber;
+    
+    @Column(name="CONTACT_EMAIL")
+    private String contactEmail;
+    
+    @Column(name="CONTACT_ACTIVESW")
+    private Character activeSw;
+    
+    @Column(name="CONTACT_CREATEDDATE")
+    private LocalDate createdDate;
+    
+    @Column(name="CONTACT_UPDATEDDATE")
+    private LocalDate updatedDate;
+
+	public int getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public Long getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(Long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public Character getActiveSw() {
+		return activeSw;
+	}
+
+	public void setActiveSw(Character activeSw) {
+		this.activeSw = activeSw;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDate getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDate updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [contactId=" + contactId + ", contactName=" + contactName + ", contactNumber=" + contactNumber
+				+ ", contactEmail=" + contactEmail + ", activeSw=" + activeSw + ", createdDate=" + createdDate
+				+ ", updatedDate=" + updatedDate + "]";
+	}
+	
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/bikkadIt/PhoneBookApplication/Service/ContactSeviceImpl.java
+++ src/main/java/com/bikkadIt/PhoneBookApplication/Service/ContactSeviceImpl.java
@@ -1,5 +1,8 @@
 package com.bikkadIt.PhoneBookApplication.Service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContactSeviceImpl {
 
 }
