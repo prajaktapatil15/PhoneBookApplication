de20bdc8e6377c9cfea5116caf12d7f2eaa3b720
prajaktapatil15 <prajaktapatil15700@gmail.com>
2022-06-23 13:22:49 +0530

    PHONE 09: write method declaration into ContactServiceI

--- src/main/java/com/bikkadIt/PhoneBookApplication/Service/ContactServiceI.java
+++ src/main/java/com/bikkadIt/PhoneBookApplication/Service/ContactServiceI.java
@@ -1,5 +1,9 @@
 package com.bikkadIt.PhoneBookApplication.Service;
 
+import com.bikkadIt.PhoneBookApplication.Contact;
+
 public interface ContactServiceI {
 
+	boolean savecontact(Contact contact);
+	
 }
